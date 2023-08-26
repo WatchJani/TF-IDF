0ac150f99c78c980a4cf6b4410321726362d2ea3
WatchJani <jankokondic84@gmail.com>
2023-08-26 22:54:11 +0200

    last check

--- main.go
+++ main.go
@@ -20,36 +20,6 @@ func init() {
 }
 
 func main() {
-	// IDF := t.NewTF_IDF()
-
-	// IDF.InitData()
-
-	// myFileForRead := f.ReadFile("./blog/test_file")
-
-	// start := time.Now()
-
-	//+ data := IDF.TF_IDF(myFileForRead)
-
-	// var keyValueList []KeyValue
-	// for k, v := range data {
-	// 	keyValueList = append(keyValueList, KeyValue{k, v})
-	// }
-
-	// sort.Slice(keyValueList, func(i, j int) bool {
-	// 	return keyValueList[i].Value > keyValueList[j].Value
-	// })
-
-	// topKeys := make([]string, 0, 8)
-	// for i := 0; i < 8 && i < len(keyValueList); i++ {
-	// 	topKeys = append(topKeys, keyValueList[i].Key)
-	// }
-
-	// fmt.Println(time.Since(start))
-
-	// fmt.Println("Best 5 tags:", topKeys)
-
-	//====================================================================================
-
 	store, err := db.Open(db.PostgresConfiguration())
 	e.ErrorHandler(err)
 
